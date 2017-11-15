faa6bbe6d45e6b9b5c606cff5b7eda52adbd9ed0
remypeyret <33670092+remypeyret@users.noreply.github.com>
2017-11-15 06:00:00 +0100

    Update Program.cs
    Amélioration du message
    Ajout de commentaires

--- ConversionNombresArabeVersRomain/ConversionNombresArabeVersRomain/Program.cs
+++ ConversionNombresArabeVersRomain/ConversionNombresArabeVersRomain/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +8,10 @@ namespace ConversionNombresArabeVersRomain
 {
     class Program
     {
-        const string msgUtilisation = "Veuillez saisir un nombre...\n";
+        const string msgUtilisation = "Veuillez saisir un nombre entier positif...\n";
         static void Main(string[] args)
         {
+            // fonctionnement en boucle infinie : la fonction demande un nombre, le converti et en demande un autre
             GererSaisie();
         }
 
@@ -22,22 +23,25 @@ namespace ConversionNombresArabeVersRomain
 
             if (!(int.TryParse(nbConsole, out nbSaisi)))
             {
+                // la saisie est invalide, on demande un nouveau nombre
                 GererSaisie();
             }
             else
             {
                 string s = ConvertirNombreEnRomain(nbSaisi);
                 Console.Write(s + "\n");
+                // on demande un nouveau nombre
                 GererSaisie();
             }
         }
 
         static string ConvertirNombreEnRomain(int nb)
         {
-            string nombreRomain = "";
+            string nombreRomain = ""; // chaine qui sera renvoyée - vide si nombre négatif
             const int nbSymboles = 7;
-            string[] symbolesRomains = new String[nbSymboles];
-            int[] nombres = new int[nbSymboles];
+            string[] symbolesRomains = new String[nbSymboles]; // tableau des symboles roamins
+            int[] nombres = new int[nbSymboles]; // tabeau des nombres corresponant aux symboles romains !les indices doivent correspondre
+            // initialisation des tableaux
             symbolesRomains[0] = "I";
             symbolesRomains[1] = "V";
             symbolesRomains[2] = "X";
@@ -52,17 +56,25 @@ namespace ConversionNombresArabeVersRomain
             nombres[4] = 100;
             nombres[5] = 500;
             nombres[6] = 1000;
-            bool test = nb > 0;
+                                   
+            
             int valeur = nb;
+            bool test = valeur > 0;
+            // boucle tant que la valeur est supérieure à 0         
             while (test)
             {
+                // pour chaque nombre correspondant à un symbole romain, du plus grand au plus petit,
                 for (int ii = nbSymboles - 1; ii >= 0; ii--)
                 {
+                    //  tant que la valeur est supérieure à ce nombre, le nombre est enlevé de la valeur et le symbole est ajouté dans la chaine de sortie
                     while (valeur >= nombres[ii])
                     {
                         nombreRomain += symbolesRomains[ii];
                         valeur -= nombres[ii];
                     }
+                    // Gestion du 9
+                    // si l'indice est supérieur à un et si la valeur est supérieure au nombre en cours moins le second plus petit (ex X et I)
+                    // le nombre en cours est enlevé de la valeur et l'autre est ajouté; les symboles - plus petit puis plus grand - sont ajoutés dans la chaine de sortie
                     if (ii > 1)
                     {
                         if (valeur >= nombres[ii] - nombres[ii - 2])
@@ -71,6 +83,9 @@ namespace ConversionNombresArabeVersRomain
                             valeur -= nombres[ii] - nombres[ii - 2];
                         }
                     }
+                    // Gestion du 4
+                    // si l'indice est supérieur à un et si la valeur est supérieure au nombre en cours moins le suivant plus petit (ex V et I)
+                    // le nombre en cours est enlevé de la valeur et l'autre est ajouté; les symboles - plus petit puis plus grand - sont ajoutés dans la chaine de sortie
                     if (ii > 0)
                     {
                         if ((valeur >= nombres[ii] - nombres[ii - 1]) && (nombres[ii] - nombres[ii - 1] != nombres[ii - 1]))
